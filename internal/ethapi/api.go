6c37de880b064ffe1ac76deb3791403e4754bb01
Ubuntu <ubuntu@ip-172-31-17-97.us-east-2.compute.internal>
2021-05-07 22:57:42 +0000

    fix chainid on rpc api

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -533,7 +533,11 @@ func NewPublicBlockChainAPI(b Backend) *PublicBlockChainAPI {
 func (api *PublicBlockChainAPI) ChainId() (*hexutil.Big, error) {
 	// if current block is at or past the EIP-155 replay-protection fork block, return chainID from config
 	if config := api.b.ChainConfig(); config.IsEIP155(api.b.CurrentBlock().Number()) {
-		return (*hexutil.Big)(config.ChainID), nil
+		if config := api.b.ChainConfig(); config.IsGENESYSFork(api.b.CurrentBlock().Number()) {
+			return (*hexutil.Big)(big.NewInt(786)), nil
+		} else {
+			return (*hexutil.Big)(config.ChainID), nil
+		}
 	}
 	return nil, fmt.Errorf("chain not synced beyond EIP-155 replay-protection fork block")
 }
